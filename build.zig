af0a87c1df0fc121fcc03bb079cc3089420d913d
Luke Briggs <22104392+LukeBriggsDev@users.noreply.github.com>
2025-01-14 22:43:08 +0000

    Add run command with build to negate need for shell script

--- build.zig
+++ build.zig
@@ -7,16 +7,24 @@ pub fn build(b: *std.Build) void {
         .abi = .none,
     });
 
-    const elf = b.addExecutable(.{
-        .name = "kernel.elf",
+    const kernel = b.addExecutable(.{
+        .name = "zix",
         .root_source_file = b.path("src/kernel.zig"),
         .target = target,
         .optimize = .Debug,
         .code_model = .medium,
     });
+    b.enable_qemu = true;
 
-    elf.linker_script = b.path("src/kernel.ld");
-    elf.entry = .{ .symbol_name = "boot" };
+    kernel.linker_script = b.path("src/kernel.ld");
+    kernel.entry = .{ .symbol_name = "boot" };
 
-    b.installArtifact(elf);
+    b.installArtifact(kernel);
+
+    const qemu_args: []const []const u8 = &.{ "qemu-system-riscv64", "-machine", "virt", "-bios", "default", "-nographic", "-serial", "mon:stdio", "--no-reboot", "-kernel", "zig-out/bin/kernel.elf" };
+
+    const qemu_cmd = b.addSystemCommand(qemu_args);
+
+    const run_step = b.step("run", "Run the application");
+    run_step.dependOn(&qemu_cmd.step);
 }
